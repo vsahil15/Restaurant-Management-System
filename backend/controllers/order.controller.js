8d74f961cb0630ed152c900bc45672c5704edec1
vasahil15 <varmasahil877@gmail>
2026-08-04 14:29:03 +0530

    order model is created

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,4 +1,11 @@
+import Order from '../models/orders.model.js';
 const addToOrder = async(req,res) => {
  const {itemName,itemPrice,quantity} = req.body;
-
+ const newOrder = new Order({
+   items:[{
+    name:itemName,
+    price:Number(itemPrice),
+    quantity:Number(quantity)
+   }]
+ })
 }
---
+++ backend/models/orders.model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+    items:[{
+        name:String,
+        price:Number,
+        quantity:Number
+    }]
+});
+
+const Order = mongoose.model('Order',orderSchema);
+
+export default Order;
