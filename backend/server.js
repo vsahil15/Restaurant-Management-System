ef671f59908ea80c3f604e9e2a01d3a160673262
Your Name <your@email.com>
2026-09-01 14:38:23 +0530

    sever health issue solve

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import config from './config/config.js';
 
 const PORT = config.PORT || 3000;
 
+await connectDb();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-await connectDb();
+
